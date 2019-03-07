302a8ce94df0566e73b14699ea2e5cd1a992352e
Benedict Weichselbaum <bmw00178@gmail.com>
2019-03-07 14:12:12 +0100

    first UI-logic

---
+++ src/sample/Logic/CalculatorLogic.java
@@ -0,0 +1,14 @@
+package sample.Logic;
+
+public class CalculatorLogic {
+
+    Information information;
+
+    public CalculatorLogic(Information info){
+        this.information = info;
+    }
+
+    public String getSolution(){
+        return "Test";
+    }
+}
---
+++ src/sample/Logic/Information.java
@@ -0,0 +1,34 @@
+package sample.Logic;
+
+public class Information {
+
+    private char from;
+    private char to;
+    private String number;
+
+    public char getFrom() {
+        return from;
+    }
+
+    public void setFrom(char from) {
+        this.from = from;
+    }
+
+    public char getTo() {
+        return to;
+    }
+
+    public void setTo(char to) {
+        this.to = to;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+}
